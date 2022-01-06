067f0c10666a220fee0c5a98e0b6bd847db8fd59
JeongHoon Park <kmclo@naver.com>
2022-01-06 18:51:00 +0900

    :lipstick: global css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { routes } from './constants';
+import { theme, GlobalStyles } from './style/global-css';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.home} exact element={<Home />} />
-        <Route path={routes.login} exact element={<Login />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.home} exact element={<Home />} />
+          <Route path={routes.login} exact element={<Login />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/reset.css';
-import './style/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
