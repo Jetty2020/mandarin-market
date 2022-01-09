7fad128bbb7a9c3fa22d59b10e167e16fd1070bc
JeongHoon Park <kmclo@naver.com>
2022-01-09 13:42:40 +0900

    :alembic: Redux state 가져오기 확인

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PageTitle from '../components/PageTitle';
+import { loginUser } from '../redux/action/user';
 
 function Home() {
+  const data = useSelector((state) => state.user);
+  console.log('home', data);
+
   return (
     <div>
       <PageTitle title="Home" />
+      <Link to="/login">to Login</Link>
       <div>Home</div>
     </div>
   );
