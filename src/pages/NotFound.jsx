c442c1db9015f252e0d444999a954fcbd52c11e5
JeongHoon Park <kmclo@naver.com>
2022-01-10 00:33:35 +0900

    :adhesive_bandage: PageTitle 변경된 경로 수정

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../components/common/PageTitle';
+
+function NotFound() {
+  return (
+    <div>
+      <PageTitle title="NotFound" />
+      <div>NotFound</div>
+    </div>
+  );
+}
+
+export default NotFound;
