fee92e205c3024c273b48b4e0d5d1ba2b965e829
JeongHoon Park <kmclo@naver.com>
2022-01-10 00:33:35 +0900

    :adhesive_bandage: PageTitle 변경된 경로 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import PageTitle from '../components/PageTitle';
+import PageTitle from '../components/common/PageTitle';
 import { loginUser } from '../redux/action/user';
 
 function Home() {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import PageTitle from '../components/PageTitle';
+import PageTitle from '../components/common/PageTitle';
 import { loginUser } from '../redux/action/user';
 
 function Login() {
