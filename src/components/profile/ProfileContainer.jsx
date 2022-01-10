51167b0d3ef7499780bc12d7b007caa3f35d17fe
chango <kimtothechang@gmail.com>
2022-01-10 10:27:54 +0900

    ✨ Profile 파일 생성

---
+++ src/components/profile/ProfileContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProfileCard from './ProfileCard';
+import SellingContainer from './SellingContainer';
+import FeedContainer from './FeedContainer';
+
+function ProfileContainer() {
+  return <div />;
+}
+
+export default ProfileContainer;
