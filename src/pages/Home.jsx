1eec312ef5d1bf113e7035ed654ab3ff58d48e1f
howcaniresistyou <76487478+howcaniresistyou@users.noreply.github.com>
2022-01-12 14:26:57 +0900

    :fire: 필요없는 코드 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/common/Navbar';
 import PageTitle from '../components/common/PageTitle';
-import { loginUser } from '../redux/action/user';
 
 function Home() {
-  const data = useSelector((state) => state.user);
-  console.log('home', data);
-
   return (
     <div>
       <PageTitle title="Home" />
-      <Link to="/login">to Login</Link>
       <div>Home</div>
       <Link to="/profile">to profile</Link>
       <Navbar />
