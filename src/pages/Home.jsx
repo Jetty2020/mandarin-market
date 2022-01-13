4cb01ce96fe0f91a5a80938fbfcef348c35990e3
howcaniresistyou <76487478+howcaniresistyou@users.noreply.github.com>
2022-01-13 14:59:41 +0900

    ✨ 홈 화면 피드 없을 때 UI 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import Navbar from '../components/common/Navbar';
 import PageTitle from '../components/common/PageTitle';
 
 function Home() {
   return (
-    <div>
+    <>
       <PageTitle title="Home" />
-      <div>Home</div>
-      <Link to="/profile">to profile</Link>
+      <Container>
+        <HomeImg src="/img/symbol-logo-gray.png" alt="로고" />
+        <HomeP>유저를 검색해 팔로우를 해보세요!</HomeP>
+        <HomeBtn to="/login">검색하기</HomeBtn>
+      </Container>
       <Navbar />
-    </div>
+    </>
   );
 }
 
 export default Home;
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: calc(100vh - 140px);
+  margin: 70px 0;
+  background: #fff;
+`;
+const HomeImg = styled.img``;
+const HomeP = styled.p`
+  margin: 10px 0;
+  color: #767676;
+  font-family: Spoqa Han Sans Neo;
+`;
+const HomeBtn = styled(Link)`
+  padding: 10px 20px;
+  border: none;
+  border-radius: 20px;
+  background: ${(props) => props.theme.red};
+  color: #fff;
+`;
