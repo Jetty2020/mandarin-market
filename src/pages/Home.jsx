01ba0379a7e43dbebd034d9d0c668f47819c880d
howcaniresistyou <76487478+howcaniresistyou@users.noreply.github.com>
2022-01-14 15:37:02 +0900

    :lipstick: 팔로잉 유무 별 UI 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from '../components/common/Navbar';
 import PageTitle from '../components/common/PageTitle';
+import FeedContainer from '../components/profile/FeedContainer';
 
 function Home() {
+  const [haveFollowing, setHaveFollowing] = useState(true);
   return (
-    <>
-      <PageTitle title="Home" />
-      <Container>
-        <HomeImg src="/img/symbol-logo-gray.png" alt="로고" />
-        <HomeP>유저를 검색해 팔로우를 해보세요!</HomeP>
-        <HomeBtn to="/login">검색하기</HomeBtn>
-      </Container>
-      <Navbar />
-    </>
+    <div>
+      {haveFollowing ? (
+        <div>
+          <FeedContainer />
+          <FeedContainer />
+          <FeedContainer />
+          <Navbar />
+        </div>
+      ) : (
+        <div>
+          <PageTitle title="Home" />
+          <Container>
+            <HomeImg src="/img/symbol-logo-gray.png" alt="로고" />
+            <HomeP>유저를 검색해 팔로우를 해보세요!</HomeP>
+            <HomeBtn to="/search">검색하기</HomeBtn>
+          </Container>
+          <Navbar />
+        </div>
+      )}
+    </div>
   );
 }
 
@@ -34,6 +47,7 @@ const HomeImg = styled.img``;
 const HomeP = styled.p`
   margin: 10px 0;
   color: #767676;
+  padding: 10px;
   font-family: Spoqa Han Sans Neo;
 `;
 const HomeBtn = styled(Link)`
