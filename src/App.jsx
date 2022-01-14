c471f8b56c93fec0dac953d1bf8c82d28fd09b7b
0cddo <0.c.dddo@gmail.com>
2022-01-14 14:28:00 +0900

    ✨ join, login-email 라우팅 연결

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { routes } from './constants';
+import { theme, GlobalStyles } from './style/global-css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import store from './redux/store';
+import LoginEmail from './pages/LoginEmail';
+import Join from './pages/Join';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <HelmetProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <BrowserRouter>
+            <Routes>
+              <Route path={routes.home} element={<Home />} />
+              <Route path={routes.login} element={<Login />} />
+              <Route path={routes.loginEmail} element={<LoginEmail />} />
+              <Route path={routes.join} element={<Join />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
+    </Provider>
+  );
+}
+
+export default App;
