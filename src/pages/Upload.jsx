17a18075736cc0d5e190258e6613ba1fbc0acdcf
Sooyoung Yoon <ayoooo240@gmail.com>
2022-01-17 00:02:15 +0900

    ✨ Upload 페이지 생성 및 Header 연결

---
+++ src/pages/Upload.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from '../components/common/PageTitle';
+import UploadHeader from '../components/upload/UploadHeader';
+
+export default function Upload() {
+  return (
+    <>
+      <PageTitle title="Upload" />
+      <UploadHeader />
+    </>
+  );
+}
