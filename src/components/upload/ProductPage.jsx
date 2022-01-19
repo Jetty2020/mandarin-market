dd4a9e7a879b7ebd5fadd05ae7f3341b2fe1ad84
Sooyoung Yoon <ayoooo240@gmail.com>
2022-01-19 17:27:52 +0900

    ✨가격 입력값 원단위 변환 기능 추가 - submit 시 다시 숫자형 변환

--- src/components/upload/ProductPage.jsx
+++ src/components/upload/ProductPage.jsx
@@ -1,21 +1,92 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
 export default function ProductPage() {
-  const onClickUpload = (event) => {
-    event.preventDefault();
-    // addProduct();
+  const {
+    register,
+    getValues,
+    setValue,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onTouched',
+  });
+  const noString = (event) => {
+    const { value } = event.target;
+    return value.replace(/[^0-9]/g, '');
+  };
+  const addComma = (event) => {
+    const regexp = /\B(?=(\d{3})+(?!\d))/g;
+    const { value } = event.target;
+    return value.toString().replace(regexp, ',');
+  };
+  const addProduct = async (data) => {
+    // console.log(data);
+    const str = await getValues('price');
+    setValue('price', parseInt(str.replace(/[^0-9]/g, ''), 10));
+    const priceNum = getValues('price');
+    // console.log(priceNum);
+    // console.log(typeof priceNum);
   };
 
   return (
-    <div>
-      <p>상품 등록 페이지</p>
-      <UploadBtn type="submit" onClick={onClickUpload} color="#F26E22">
-        저장
-      </UploadBtn>
-    </div>
+    <ProductContainer>
+      <form onSubmit={handleSubmit(addProduct)}>
+        <InputWrapper>
+          <Label htmlFor="productName">상품명</Label>
+          <Input
+            type="text"
+            spellCheck="false"
+            autoComplete="off"
+            placeholder="2-10자 이내여야 합니다."
+            {...register('productName', {
+              required: true,
+              minLength: 2,
+              maxLength: 10,
+            })}
+          />
+          {errors.productName && errors.productName.type === 'minLength' && (
+            <Error>* 최소 2자 이상이어야 합니다.</Error>
+          )}
+          {errors.productName && errors.productName.type === 'maxLength' && (
+            <Error>* 최대 10자 이하여야 합니다.</Error>
+          )}
+        </InputWrapper>
+        <InputWrapper>
+          <Label htmlFor="price">가격</Label>
+          <Input
+            type="text"
+            spellCheck="false"
+            autoComplete="off"
+            placeholder="숫자만 입력 가능합니다."
+            {...register('price', {
+              required: true,
+              onChange: (e) => {
+                setValue('price', noString(e));
+              },
+              onBlur: (e) => {
+                setValue('price', addComma(e));
+              },
+            })}
+          />
+        </InputWrapper>
+        <UploadBtn
+          type="submit"
+          disabled={!isValid}
+          color={isValid ? '#F26E22' : '#FFC7A7'}
+        >
+          저장
+        </UploadBtn>
+      </form>
+    </ProductContainer>
   );
 }
+
+const ProductContainer = styled.main`
+  padding: 30px 34px;
+  margin-top: 48px;
+`;
 
 const UploadBtn = styled.button`
   position: absolute;
@@ -32,4 +103,40 @@ const UploadBtn = styled.button`
   transition: all 0.4s ease;
   z-index: 150;
   cursor: ${(props) => (props.disabled === true ? 'default' : 'pointer')};
+`;
+
+const InputWrapper = styled.div`
+  margin-top: 16px;
+  border: 1px solid #eee;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 10px;
+  color: ${(props) => props.theme.gray};
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 23px;
+  padding-bottom: 8px;
+  border: none;
+  border-bottom: 1px solid #dbdbdb;
+  line-height: 14px;
+  transition: all 0.4s ease;
+  :focus {
+    border-bottom: 1px solid ${(props) => props.theme.accent};
+  }
+  ::placeholder {
+    color: #dbdbdb;
+    font-size: 14px;
+  }
+`;
+
+const Error = styled.p`
+  margin-top: 6px;
+  color: #eb5757;
+  font-size: 12px;
 `;
