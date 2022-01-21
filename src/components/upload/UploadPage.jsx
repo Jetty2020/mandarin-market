2b7ab7053c4acff83297086c35c3622a4994c0c0
Sooyoung Yoon <ayoooo240@gmail.com>
2022-01-22 06:33:07 +0900

    ✨ 게시글 작성 이미지 첨부 기능 구현

--- src/components/upload/UploadPage.jsx
+++ src/components/upload/UploadPage.jsx
@@ -45,8 +45,38 @@ export default function UploadPage() {
     getProfile();
   }, []);
 
+  const [addedImgUrl, setAddedImgUrl] = useState('');
   const [imageArr, setImageArr] = useState([]);
-  const imageUrls = imageArr.toString();
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const imageUpload = async (files) => {
+    const formData = new FormData();
+    formData.append('image', files[0]);
+    const uploadedImg = await (
+      await axios.post(`${SERVER_BASE_URL}/image/uploadfile`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      })
+    ).data;
+    const imgName = uploadedImg.filename;
+    setAddedImgUrl(`${SERVER_BASE_URL}/${imgName}`);
+  };
+  const addImage = (event) => {
+    const files = [...event.target.files];
+    imageUpload(files);
+  };
+
+  useEffect(() => {
+    if (addedImgUrl !== '') {
+      setImageArr((currentArr) => [...currentArr, addedImgUrl]);
+      setAddedImgUrl('');
+    } else if (addedImgUrl === '') {
+      setImageUrls(imageArr.toString());
+    }
+  }, [addedImgUrl]);
+
   const uploadPost = async () => {
     const postData = {
       post: {
@@ -73,37 +103,55 @@ export default function UploadPage() {
   };
 
   return (
-    <UploadContainer>
-      <ProfileImage src={profileImg} alt="프로필 사진" />
-      <PostForm>
-        <PostTextArea
-          ref={ref}
-          onChange={onChange}
-          onInput={resizeHeight}
-          value={typed}
-          placeholder="게시글 입력하기..."
-          spellCheck="false"
-        />
-        <UploadBtn
-          type="submit"
-          onClick={onClickUpload}
-          color={typed ? '#F26E22' : '#FFC7A7'}
-          disabled={!typed}
-        >
-          업로드
-        </UploadBtn>
-        <UploadImgBtn htmlFor="uploadImage">
-          <img src="../img/upload-file.png" alt="이미지 첨부" />
-          <input
-            type="file"
-            title=" "
-            accept="image/*"
-            id="uploadImage"
-            multiple
+    <>
+      <UploadContainer>
+        <PageTitle>게시글 등록 페이지</PageTitle>
+        <ProfileImage src={profileImg} alt="프로필 사진" />
+        <PostForm>
+          <PostTextArea
+            ref={ref}
+            onChange={onChange}
+            onInput={resizeHeight}
+            value={typed}
+            placeholder="게시글 입력하기..."
+            spellCheck="false"
           />
-        </UploadImgBtn>
-      </PostForm>
-    </UploadContainer>
+          <UploadBtn
+            type="submit"
+            onClick={onClickUpload}
+            color={typed ? '#F26E22' : '#FFC7A7'}
+            disabled={!typed}
+          >
+            업로드
+          </UploadBtn>
+          <UploadImgBtn htmlFor="uploadImage">
+            <img src="../img/upload-file.png" alt="이미지 첨부" />
+            <input
+              type="file"
+              title=" "
+              accept="image/*"
+              id="uploadImage"
+              onChange={addImage}
+            />
+          </UploadImgBtn>
+        </PostForm>
+      </UploadContainer>
+      {imageArr && (
+        <PhotoContainer>
+          <SectionTitle>첨부한 이미지 목록</SectionTitle>
+          <PhotoList>
+            {imageArr.map((photo) => (
+              <PhotoItem key={photo}>
+                <img src={photo} alt="" />
+                <DeleteBtn type="button">
+                  <BtnText>이미지 삭제</BtnText>
+                </DeleteBtn>
+              </PhotoItem>
+            ))}
+          </PhotoList>
+        </PhotoContainer>
+      )}
+    </>
   );
 }
 
@@ -111,6 +159,17 @@ const UploadContainer = styled.main`
   display: flex;
   margin-top: 48px;
   padding: 20px 16px;
+`;
+
+const PageTitle = styled.h2`
+  overflow: hidden;
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  border: 0;
+  clip: rect(0, 0, 0, 0);
 `;
 
 const ProfileImage = styled.img`
@@ -174,4 +233,62 @@ const UploadImgBtn = styled.label`
     border: 0;
     clip: rect(0, 0, 0, 0);
   }
+`;
+
+const PhotoContainer = styled.section`
+  display: flex;
+  overflow-x: scroll;
+  width: calc(100% - 70px);
+  margin-left: 70px;
+`;
+
+const SectionTitle = styled.h3`
+  overflow: hidden;
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  border: 0;
+  clip: rect(0, 0, 0, 0);
+`;
+
+const PhotoList = styled.ul`
+  display: flex;
+  gap: 8px;
+`;
+
+const PhotoItem = styled.li`
+  overflow: hidden;
+  position: relative;
+  width: 168px;
+  height: 126px;
+  border-radius: 10px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const DeleteBtn = styled.button`
+  position: absolute;
+  top: 11px;
+  right: 11px;
+  width: 11px;
+  height: 11px;
+  border: none;
+  background: url(img/icon/icon-delete.svg) center no-repeat;
+  cursor: pointer;
+`;
+
+const BtnText = styled.span`
+  overflow: hidden;
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  border: 0;
+  clip: rect(0, 0, 0, 0);
 `;
