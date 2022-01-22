fac160d796eacba5fa1f95b9c178f40c7fa458f1
0cddo <0.c.dddo@gmail.com>
2022-01-22 14:45:45 +0900

    ✨ login API 연결 및 로그인된 유저 예외처리

--- src/components/login/LoginEmail.jsx
+++ src/components/login/LoginEmail.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
-import { routes } from '../../constants';
+import axios from 'axios';
+import { routes, SERVER_BASE_URL } from '../../constants';
 
 export default function LoginEmail() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (window.localStorage.getItem('token')) {
+      navigate('/');
+    }
+  }, []);
+
+  const [loginError, setLoginError] = useState('');
   const {
     register,
     handleSubmit,
@@ -12,7 +21,27 @@ export default function LoginEmail() {
   } = useForm({
     mode: 'onChange',
   });
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const response = await axios(`${SERVER_BASE_URL}/user/login/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify({
+        user: {
+          email: data.email,
+          password: data.password,
+        },
+      }),
+    });
+    if (response.data.message) {
+      setLoginError(response.data.message);
+    } else {
+      window.localStorage.setItem('account', response.data.user.accountname);
+      window.localStorage.setItem('token', response.data.user.token);
+      navigate('/');
+    }
+  };
   return (
     <Container>
       <h1>로그인</h1>
@@ -32,6 +61,9 @@ export default function LoginEmail() {
           {errors.email && errors.email.type === 'required' && (
             <Error>* 이메일을 입력해주세요</Error>
           )}
+          {errors.email && errors.email.type === 'pattern' && (
+            <Error>* 잘못된 이메일 형식입니다.</Error>
+          )}
         </InputWrapper>
 
         <InputWrapper>
@@ -45,11 +77,7 @@ export default function LoginEmail() {
           {errors.password && errors.password.type === 'required' && (
             <Error>* 비밀번호를 입력해주세요</Error>
           )}
-          {/* 이메일 또는 비밀번호를 입력하세요 */}
-          {/* {(errors.email || errors.password) &&
-            (errors.email.type === 'required' ||
-              errors.password.type === 'required') && <Error>gg</Error>} */}
-          {/* 이메일 또는 비밀번호가 일치하지 않습니다. */}
+          {loginError && <Error> * {loginError}</Error>}
         </InputWrapper>
         <LoginBtn type="submit" disabled={!isValid}>
           로그인
@@ -124,7 +152,7 @@ const RegisterLink = styled.p`
 `;
 
 const Error = styled.p`
-  margin: 6px 0 16px;
+  margin: 6px 0 0;
   font-size: 12px;
   color: #eb5757;
 `;
