7bae1d1823899f9765bb9f0539657939e92c3b7f
JeongHoon Park <kmclo@naver.com>
2022-01-23 17:23:32 +0900

    :fire: App.jsx에서 redux 관련 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { routes } from './constants';
 import { theme, GlobalStyles } from './style/global-css';
@@ -11,7 +10,6 @@ import Profile from './pages/Profile';
 import Chat from './pages/Chat';
 import ChatRoom from './pages/ChatRoom';
 import NotFound from './pages/NotFound';
-import store from './redux/store';
 import LoginEmail from './pages/LoginEmail';
 import Join from './pages/Join';
 import Upload from './pages/Upload';
@@ -26,36 +24,34 @@ import Search from './pages/Search';
 
 function App() {
   return (
-    <Provider store={store}>
-      <HelmetProvider>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <BrowserRouter>
-            <Routes>
-              <Route path={routes.postDetail} element={<PostDetail />} />
-              <Route path={routes.edit} element={<Edit />} />
-              <Route path={routes.following} element={<Following />} />
-              <Route path={routes.follower} element={<Follower />} />
-              <Route path={routes.ownProfile} element={<Profile />} />
-              <Route path={routes.home} element={<Home />} />
-              <Route path={routes.login} element={<Login />} />
-              <Route path={routes.profile} element={<Profile />} />
-              <Route path={routes.search} element={<Search />} />
-              <Route path={routes.ownProfile} element={<Profile />} />
-              <Route path={routes.chat} element={<Chat />} />
-              <Route path={routes.chatRoom} element={<ChatRoom />} />
-              <Route path={routes.loginEmail} element={<LoginEmail />} />
-              <Route path={routes.join} element={<Join />} />
-              <Route path={routes.upload} element={<Upload />} />
-              <Route path={routes.uploadEdit} element={<UploadEdit />} />
-              <Route path={routes.product} element={<Product />} />
-              <Route path={routes.productEdit} element={<ProductEdit />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </ThemeProvider>
-      </HelmetProvider>
-    </Provider>
+    <HelmetProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path={routes.postDetail} element={<PostDetail />} />
+            <Route path={routes.edit} element={<Edit />} />
+            <Route path={routes.following} element={<Following />} />
+            <Route path={routes.follower} element={<Follower />} />
+            <Route path={routes.ownProfile} element={<Profile />} />
+            <Route path={routes.home} element={<Home />} />
+            <Route path={routes.login} element={<Login />} />
+            <Route path={routes.profile} element={<Profile />} />
+            <Route path={routes.search} element={<Search />} />
+            <Route path={routes.ownProfile} element={<Profile />} />
+            <Route path={routes.chat} element={<Chat />} />
+            <Route path={routes.chatRoom} element={<ChatRoom />} />
+            <Route path={routes.loginEmail} element={<LoginEmail />} />
+            <Route path={routes.join} element={<Join />} />
+            <Route path={routes.upload} element={<Upload />} />
+            <Route path={routes.uploadEdit} element={<UploadEdit />} />
+            <Route path={routes.product} element={<Product />} />
+            <Route path={routes.productEdit} element={<ProductEdit />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
 
