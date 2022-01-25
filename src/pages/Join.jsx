66836f2bf0e7f006847dcfa4f90eeeabbc7e50d9
0cddo <0.c.dddo@gmail.com>
2022-01-25 20:50:00 +0900

    :sparkles: Join page API 연결

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PageTitle from '../components/common/PageTitle';
 import JoinMember from '../components/join/JoinMember';
 import JoinProfile from '../components/join/JoinProfile';
@@ -6,10 +7,21 @@ import JoinProfile from '../components/join/JoinProfile';
 // 함수명 중복 여부 헷갈림
 function Join() {
   const [page, setPage] = useState(true);
+  const [userInfo, setUserInfo] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (window.localStorage.getItem('token')) {
+      navigate('/');
+    }
+  }, []);
   return (
     <>
       <PageTitle title="Join" />
-      {page ? <JoinMember setPage={setPage} /> : <JoinProfile />}
+      {page ? (
+        <JoinMember setPage={setPage} setUserInfo={setUserInfo} />
+      ) : (
+        <JoinProfile userInfo={userInfo} />
+      )}
     </>
   );
 }
